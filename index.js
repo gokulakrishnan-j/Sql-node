8ec415ba5ce4cf4003cb3ae7a40679d0828ebd66
GOKULAKRISHNAN . J <110898672+gokulakrishnan-j@users.noreply.github.com>
2023-01-08 15:59:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import cors from 'cors'
 dotenv.config()
 var app =express()
 
-var Mongo_Url =process.env.Mongo_Url
+var Mongo_Url ="mongodb+srv://gokulakrishnan:Gokul4580@cluster0.lsiocek.mongodb.net"
 var client = new MongoClient(Mongo_Url)
 await client.connect()
 app.use(cors())

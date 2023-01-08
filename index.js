8ef900a7e2c816d9de3561ef0be6a52264f7259b
GOKULAKRISHNAN . J <110898672+gokulakrishnan-j@users.noreply.github.com>
2023-01-08 15:28:54 +0530

    sql

---
+++ index.js
@@ -0,0 +1,22 @@
+import express from "express"
+import { MongoClient } from "mongodb"
+import * as dotenv from "dotenv"
+import cors from 'cors'
+
+dotenv.config()
+var app =express()
+const PORT =process.env.PORT || 4000
+var Mongo_Url = process.env.Mongo_Url
+var client = new MongoClient(Mongo_Url)
+client.connect()
+app.use(cors())
+
+app.get("/",async function(request,response){
+    var data = await client
+    .db("SQL_Bolt")
+    .collection("image")
+    .find({})
+    .toArray()
+    response.send(data)
+})
+app.listen(PORT)
